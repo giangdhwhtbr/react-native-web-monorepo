c751738fc91515b0b06d8fc46db43744c9925fde
Giang <giang@kiuglobal.com>
2020-06-17 17:12:39 +0700

    add clean script, fix tsconfig, add tslint

--- packages/web/src/index.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_native_1 = require("react-native");
-const App_1 = require("@myprj/components/src/App");
-react_native_1.AppRegistry.registerComponent('myprj', () => App_1.App);
-react_native_1.AppRegistry.runApplication('myprj', {
-    rootTag: document.getElementById('root'),
-});
-//# sourceMappingURL=index.jsx.map
